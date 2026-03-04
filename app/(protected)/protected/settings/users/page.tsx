05096180fec993872ecd135b3c37022f1decb8c2
Hunter Patarapol <hunter.patarapol@gmail.com>
2026-03-04 13:16:51 +0700

    แก้ ตาราง user

--- app/(protected)/protected/settings/users/page.tsx
+++ app/(protected)/protected/settings/users/page.tsx
@@ -73,7 +73,7 @@ export default async function UsersManagementPage() {
     users?.filter((u) => u.role === "USER" || !u.role).length || 0;
 
   return (
-    <div className="max-w-7xl mx-auto p-4 md:p-8 space-y-10">
+    <div className="max-w-screen-2xl mx-auto p-4 md:p-8 space-y-10">
       {/* Header */}
       <UsersPageHeader />
 
