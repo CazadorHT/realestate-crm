2381f47d430ffb96064c04237ff63f99d15f0cf4
Mac-hunter <hunter.patarapol@gmail.com>
2026-03-30 21:43:42 +0700

    ปรับ card social

--- app/(protected)/protected/settings/page.tsx
+++ app/(protected)/protected/settings/page.tsx
@@ -101,7 +101,7 @@ export default async function SettingsPage({
           </TabsContent>
 
           <TabsContent value="social" className="space-y-6">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
               <Card
                 className={
                   isLineConnected
