4cbf1c9b4fa114dae2967392e4629c2b9e90393b
Mac-hunter <hunter.patarapol@gmail.com>
2026-07-19 00:07:44 +0700

    style: update hero section background color, brightness, and default image path

--- components/public/project-detail/ProjectHero.tsx
+++ components/public/project-detail/ProjectHero.tsx
@@ -21,10 +21,10 @@ export function ProjectHero({
   };
 
   return (
-    <section className="relative overflow-hidden mt-10 pt-20 pb-12 md:pt-32 md:pb-20 text-white bg-slate-950">
+    <section className="relative overflow-hidden pt-20 pb-12 md:pt-32 md:pb-20 text-white bg-slate-800">
       <div 
-        className="absolute inset-0 bg-cover bg-center opacity-30 blur-xs brightness-50"
-        style={{ backgroundImage: `url(${project.imageUrl || "/images/hero-projects.jpg"})` }}
+        className="absolute inset-0 bg-cover bg-center  blur-xs brightness-60"
+        style={{ backgroundImage: `url(${project.imageUrl || "/images/hero-projects.png"})` }}
       />
       <div className="absolute inset-0 bg-linear-to-t from-slate-900 via-slate-900/60 to-transparent" />
 
